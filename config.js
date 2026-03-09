64e30f2ed7c330aab1f1fc1705b07cf2a07b57da
abishek-oss <abishek@amzprep.com>
2026-03-09 19:34:53 -0400

    Update config.js

--- config.js
+++ config.js
@@ -6,14 +6,14 @@ const CONFIG = {
   //
   quantities: [
     { key: "SKUs",     label: "SKUs",      value: 2000,  show: true  },
-    { key: "Orders",     label: "First pick",      value: 2000,  show: true  },
-    { key: "Second Picks",     label: "Second Pick",      value: 200,  show: true  },
-     { key: "Inserts",     label: "inserts",      value: 0,  show: false  },
-    { key: "Shipping",    label: "Orders",            value: 2000,  show: true  },
-    { key: "containers", label: "Containers", value: 20,     show: true },
-    { key: "Pallets", label: "pallets", value: 0,     show: false },
-    { key: "Inbound Items",   label: "Inbound",          value: 1100,     show: true },
-    { key: "Supplies",     label: "supplies",      value: 2000,  show: true  },
+    { key: "First pick",     label: "Order Fee + First pick",      value: 2000,  show: true  },
+    { key: "Second Pick",     label: "Second Pick",      value: 200,  show: true  },
+     { key: "inserts",     label: "Inserts",      value: 0,  show: false  },
+    { key: "Shipping",    label: "Shipping",            value: 2000,  show: true  },
+    { key: "containers", label: "Container Destuff", value: 20,     show: true },
+    { key: "Pallets", label: "Pallets", value: 0,     show: false },
+    { key: "Inbound",   label: "Inbound Items",          value: 1100,     show: true },
+    { key: "Supplies",     label: "Supplies",      value: 2000,  show: true  },
   ],
 
   // ── Line Items ─────────────────────────────────────────────
@@ -28,10 +28,10 @@ const CONFIG = {
     { label: "Order Fee and 1st Pick",        qtyKey: "First pick",     rate: 1.95,   show: true  },
     { label: "2nd+ Pick Fee",                 qtyKey: "Second Pick",     rate: 0.55,   show: true  },
     { label: "Inserts",                 qtyKey: "inserts",     rate: 0.20,   show: false  },
-    { label: "Shipping",                 qtyKey: "Orders",     rate: 8.00,  show: true  },
-    { label: "Pallets Stored",           qtyKey: "pallets",    rate: 23.50,  show: false  },
-    { label: "Container Destuff (40ft)", qtyKey: "Containers", rate: 600.00, show: true  },
-    { label: "Supplies",          qtyKey: "supplies",   rate: 1.00,   show: true  },
+    { label: "Shipping",                 qtyKey: "Shipping",     rate: 8.00,  show: true  },
+    { label: "Pallets Stored",           qtyKey: "Pallets",    rate: 23.50,  show: false  },
+    { label: "Container Destuff (40ft)", qtyKey: "containers", rate: 600.00, show: true  },
+    { label: "Supplies",          qtyKey: "Supplies",   rate: 1.00,   show: true  },
     { label: "Inbound per Item",          qtyKey: "Inbound",   rate: 0.30,   show: true  },
   ],
 
